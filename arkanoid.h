cc604b6544c94d75053f75beca4e15de5585b56f
Alexandre <aofferle@cfai-formation.fr>
2024-02-21 11:02:03 +0100

    first commit

---
+++ arkanoid.h
@@ -0,0 +1,10 @@
+//
+// Created by alexa on 2/21/2024.
+//
+
+#ifndef ARKANOID_H
+#define ARKANOID_H
+
+/* fonction qui met à jour la surface de la fenetre "win_surf" */
+void draw();
+#endif //ARKANOID_H
